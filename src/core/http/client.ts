64f324aa97efd72c1750c9aca435d86240f8c277
João Alberto <hey@joaoalberto.dev>
2024-10-25 17:44:51 -0300

    chore: setup http client

---
+++ src/core/http/client.ts
@@ -0,0 +1,33 @@
+type FetchOptions = RequestInit;
+
+async function httpClient<T>(
+  url: string,
+  options: FetchOptions = {},
+): Promise<T> {
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}, statusText: ${response.statusText}`);
+
+    const contentType = response.headers.get("content-type");
+
+    if (contentType && contentType.includes("application/json")) {
+      const data: T = await response.json();
+
+      return data;
+    } else {
+      throw new Error(`Unexpected content type: ${contentType}`);
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+function createHttpClient(baseUrl: string) {
+  return {
+    get: <T>(path: string, options: FetchOptions = {}) =>
+      httpClient<T>(baseUrl + path, { ...options, method: 'GET' }),
+  };
+}
+
+export { createHttpClient, httpClient };
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@
 
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     globals: true,
     environment: "jsdom",
