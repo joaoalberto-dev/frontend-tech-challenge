81a7f2e702bc14ce5e0a9e0993154847d937ab57
João Alberto <hey@joaoalberto.dev>
2024-10-29 13:01:49 -0300

    chore: adding test utils

---
+++ src/core/test-utils/index.tsx
@@ -0,0 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { render, within, RenderOptions } from "@testing-library/react";
+import { ReactElement } from "react";
+
+const customScreen = within(document.body);
+const customWithin = (element: HTMLElement) => within(element);
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "queries">
+) =>
+  render(ui, {
+    wrapper: ({ children }) => {
+      const client = new QueryClient();
+      return (
+        <QueryClientProvider client={client}>{children}</QueryClientProvider>
+      );
+    },
+    ...options,
+  });
+
+export * from "@testing-library/react";
+export {
+  customScreen as screen,
+  customWithin as within,
+  customRender as render,
+};
