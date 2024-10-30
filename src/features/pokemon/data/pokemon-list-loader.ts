ac313306bff47590c51d0287ad14c56f33820b6a
João Alberto <hey@joaoalberto.dev>
2024-10-30 16:52:10 -0300

    feat: adding pokemon list loader

---
+++ src/features/pokemon/data/pokemon-list-loader.ts
@@ -0,0 +1,24 @@
+import { pokemonApi } from "@/core/services/pokemon-api";
+import { PokemonIndexListResponse } from "@/core/services/pokemon-api.types";
+
+async function pokemonListLoader() {
+  try {
+    let pokemonList: PokemonIndexListResponse = JSON.parse(
+      window.localStorage.getItem("POKEMON_LIST") ?? "{}"
+    );
+
+    if (!pokemonList) {
+      pokemonList = await pokemonApi.get<PokemonIndexListResponse>(
+        "/pokemon?limit=10000"
+      );
+
+      window.localStorage.setItem("POKEMON_LIST", JSON.stringify(pokemonList));
+    }
+
+    return pokemonList.results;
+  } catch {
+    return [];
+  }
+}
+
+export { pokemonListLoader };
