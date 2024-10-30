d1287a1051616fadd5b37a7239d16ccbd0c3e48c
João Alberto <hey@joaoalberto.dev>
2024-10-30 16:44:14 -0300

    chore: delete unused componets

--- src/features/poke-trainer-list/ui/empty-list/empty-list.tsx
+++
@@ -1,9 +0,0 @@
-function EmptyList() {
-  return (
-    <div className="w-full flex bg-neutral-100 p-10 items-center justify-center rounded-md">
-      <p>No trainers found.</p>
-    </div>
-  );
-}
-
-export { EmptyList };
