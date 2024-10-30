939ac9f7848b9285d844ae63a88d5ce1456adea1
João Alberto <hey@joaoalberto.dev>
2024-10-30 16:58:38 -0300

    feat: fetch pokemon detail

---
+++ src/features/poke-trainer-detail/data/get-poke-trainer-detail.ts
@@ -0,0 +1,16 @@
+import { RickAndMortyCharacter } from "@/core/services/rick-and-morty.types";
+import { rickAndMortyApi } from "@/core/services/rick-and-morty";
+
+async function getPokeTrainerDetail(
+  id: string
+): Promise<RickAndMortyCharacter> {
+  try {
+    const response = await rickAndMortyApi.get(`/character/${id}`);
+
+    return response as RickAndMortyCharacter;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export { getPokeTrainerDetail };
