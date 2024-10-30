e666e1b49bcbd5aec23a34c4cdda136d7097b5ee
João Alberto <hey@joaoalberto.dev>
2024-10-30 17:06:24 -0300

    chore: render poke-trainer detail page

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./core/components/layout";
+import { PokeTrainerDetail } from "./features/poke-trainer-detail/ui/page";
+import { pokemonListLoader } from "./features/pokemon/data/pokemon-list-loader";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    loader: pokemonListLoader,
     children: [
       {
-        path: "/poke-trainers",
+        index: true,
         element: <PokeTrainerListPage />,
+      },
+      {
+        path: "/:id",
+        element: <PokeTrainerDetail />,
       },
     ],
   },
