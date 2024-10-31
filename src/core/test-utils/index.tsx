d9eb4b60daa3c153d6689ca3b66f5d361be283e0
João Alberto <hey@joaoalberto.dev>
2024-10-31 16:59:50 -0300

    chore: adding missing poke-trainer-list page tests

--- src/core/test-utils/index.tsx
+++ src/core/test-utils/index.tsx
@@ -1,18 +1,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, within, RenderOptions } from "@testing-library/react";
 import { ReactElement } from "react";
+import { MemoryRouter } from "react-router-dom";
 
 const customScreen = within(document.body);
 const customWithin = (element: HTMLElement) => within(element);
 const customRender = (
   ui: ReactElement,
-  options?: Omit<RenderOptions, "queries">,
+  options?: Omit<RenderOptions, "queries">
 ) =>
   render(ui, {
     wrapper: ({ children }) => {
       const client = new QueryClient();
       return (
-        <QueryClientProvider client={client}>{children}</QueryClientProvider>
+        <QueryClientProvider client={client}>
+          <MemoryRouter>{children}</MemoryRouter>
+        </QueryClientProvider>
       );
     },
     ...options,
