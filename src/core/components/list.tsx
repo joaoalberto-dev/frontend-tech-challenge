1559a7fa8f6969f3a2d1a4529166a4dcb3780214
João Alberto <hey@joaoalberto.dev>
2024-10-31 01:06:31 -0300

    chore: adding support for custom classnames

--- src/core/components/list.tsx
+++ src/core/components/list.tsx
@@ -3,11 +3,18 @@ import { ReactNode } from "react";
 type ListProps<T> = {
   items: T[];
   renderItem: (item: T) => ReactNode;
+  containerClassName?: string;
 };
 
-function List<T = unknown>({ items, renderItem }: ListProps<T>) {
+function List<T = unknown>({
+  items,
+  renderItem,
+  containerClassName = "",
+}: ListProps<T>) {
   return (
-    <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:grid-cols-4 auto-rows-auto">
+    <div
+      className={`w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 xl:grid-cols-4 auto-rows-auto ${containerClassName}`}
+    >
       {items.map((item) => {
         return renderItem(item);
       })}
