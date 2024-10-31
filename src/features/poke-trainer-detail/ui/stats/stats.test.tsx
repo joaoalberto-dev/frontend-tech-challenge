c26f09b4688498ca21c895e85459ed1d8b6bfb6b
João Alberto <hey@joaoalberto.dev>
2024-10-31 19:30:44 -0300

    chore: adding missing poke-trainer-detail stats tests

---
+++ src/features/poke-trainer-detail/ui/stats/stats.test.tsx
@@ -0,0 +1,26 @@
+import { render } from "@testing-library/react";
+import { PokeTrainerStats } from "./stats";
+import { RickAndMortyCharacter } from "@/core/services/rick-and-morty.types";
+import { describe, expect, test } from "vitest";
+
+const mockTrainer = {
+  name: "Rick Sanchez",
+  gender: "Male",
+  origin: { name: "Earth", url: "" },
+  species: "Human",
+} as unknown as RickAndMortyCharacter;
+
+describe("PokeTrainerStats Component", () => {
+  test("renders correctly with trainer details", () => {
+    const { getByText } = render(<PokeTrainerStats trainer={mockTrainer} />);
+
+    expect(getByText("Name")).toBeInTheDocument();
+    expect(getByText("Rick Sanchez")).toBeInTheDocument();
+    expect(getByText("Gender")).toBeInTheDocument();
+    expect(getByText("Male")).toBeInTheDocument();
+    expect(getByText("Origin")).toBeInTheDocument();
+    expect(getByText("Earth")).toBeInTheDocument();
+    expect(getByText("Species")).toBeInTheDocument();
+    expect(getByText("Human")).toBeInTheDocument();
+  });
+});
