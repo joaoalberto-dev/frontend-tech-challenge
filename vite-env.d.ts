948e0fa81badb675399cb46ba774a39b75d4f777
João Alberto <hey@joaoalberto.dev>
2024-10-31 02:17:19 -0300

    fix: remove duplicated file

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_RICK_AND_MORTY_BASE_API_URL: string;
-  readonly VITE_POKEMON_BASE_API_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
