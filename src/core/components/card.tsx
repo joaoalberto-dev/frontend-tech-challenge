36f2a70dbb1b8fb50e37d7628ef3d42220a71018
João Alberto <hey@joaoalberto.dev>
2024-11-12 14:35:13 -0300

    chore: improve trainer readabillity

--- src/core/components/card.tsx
+++ src/core/components/card.tsx
@@ -49,7 +49,7 @@ function Card({
         src={image}
         className="absolute inset-0 object-cover w-full h-full bg-white"
       />
-      <div className="absolute p-4 from-neutral-300 to-[50%] flex items-end text-white to-transparent bg-gradient-to-t inset-0 w-full bottom-0">
+      <div className="absolute p-4 from-neutral-600 to-[50%] flex items-end text-white to-transparent bg-gradient-to-t inset-0 w-full bottom-0">
         <p className="text-xs font-bold">{name}</p>
       </div>
     </Link>
